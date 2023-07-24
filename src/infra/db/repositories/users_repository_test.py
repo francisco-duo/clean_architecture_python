c2236131cac9a2adcaa1def665f72de743c178ea
Francisco Duó <francisco.duo2000@gmail.com>
2023-07-24 16:33:21 -0300

    feat: Implementing a function to insert users and select users

--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -1,6 +1,13 @@
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBConnectionHandler
 from .users_repository import UsersRepository
 
+db_connection_handler = DBConnectionHandler()
+connection = db_connection_handler.get_engine().connect()
 
+
+@pytest.mark.skip(reason='Sensive test')
 def test_insert_user():
     mocked_first_name = 'first'
     mocked_last_name = 'last'
@@ -12,3 +19,31 @@ def test_insert_user():
         mocked_last_name,
         mocked_age
     )
+
+    sql = '''
+        SELECT * FROM users
+        WHERE first_name = '{}'
+        AND last_name = '{}'
+        AND age = '{}'
+    '''.format(
+        mocked_first_name,
+        mocked_last_name,
+        mocked_age
+    )
+
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(
+        f'''
+            DELETE FROM users WHERE id = {registry.id}
+        '''
+    ))
+    connection.commit()
+
+
+def test_select_user(): ...
