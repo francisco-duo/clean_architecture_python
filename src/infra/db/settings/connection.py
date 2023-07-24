eeee4e75b894238ef2818ba7acb309378fd26f0e
Francisco Duó <francisco.duo2000@gmail.com>
2023-07-24 14:08:55 -0300

    feat: Implementing UserRepository

--- src/infra/db/settings/connection.py
+++ src/infra/db/settings/connection.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 class DBConnectionHandler:
@@ -13,6 +14,18 @@ class DBConnectionHandler:
             "clean_database"
         )
         self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __enter__(self, ):
+        session_make = sessionmaker(
+            bind=self.__engine
+        )
+        self.session = session_make()
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
 
     def __create_database_engine(self, ):
         engine = create_engine(self.__connection_string)
